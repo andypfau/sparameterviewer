6e70c63f32a8a05aecce94a0b41e5650a85ef6ed
Andreas Pfau <andreas.pfau@apfau.de>
2022-06-10 09:33:07 +0200

    simplified code

--- src/lib/stabcircle.py
+++ src/lib/stabcircle.py
@@ -23,21 +23,19 @@ class StabilityCircle:
         s21 = nw.s[0,1,0]
         s12 = nw.s[0,0,1]
         s22 = nw.s[0,1,1]
+
+        if port==1:
+            s11, s21, s12, s22 = s22, s12, s21, s11 # just flip the network, then do the same calculation
+        elif port!=2:
+            raise ValueError('Stability circle argument <port> must be 1 or 2')
         
+        # calculate stability circle for output
         # see <https://eng.libretexts.org/Bookshelves/Electrical_Engineering/Electronics/Microwave_and_RF_Design_V%3A_Amplifiers_and_Oscillators_(Steer)/02%3A_Linear_Amplifiers/2.06%3A_Amplifier_Stability>
         delta = s11*s22 - s12*s21
-        if port==1:
-            denom = pow(abs(s11),2) - pow(abs(delta),2)
-            self.center = (s11 - s22.conjugate()*delta).conjugate() / denom
-            self.radius = abs((s12*s21) / denom)
-            self.stable_inside = True if abs(s11)>1 else False
-        elif port==2:
-            denom = pow(abs(s22),2) - pow(abs(delta),2)
-            self.center = (s22 - s11.conjugate()*delta).conjugate() / denom
-            self.radius = abs((s12*s21) / denom)
-            self.stable_inside = True if abs(s22)>1 else False
-        else:
-            raise ValueError('Stability circle argument <port> must be 1 or 2')
+        denom = pow(abs(s22),2) - pow(abs(delta),2)
+        self.center = (s22 - s11.conjugate()*delta).conjugate() / denom
+        self.radius = abs((s12*s21) / denom)
+        self.stable_inside = True if abs(s22)>1 else False
     
 
     def get_plot_data(self, n_points) -> "np.ndarray":
