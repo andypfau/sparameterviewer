b8ca6df5a95a1fa09071c9295a0a0d8744b2bd0b
Andreas Pfau <andreas.pfau@apfau.de>
2024-07-19 12:20:41 +0200

    worked on enhanching tabular dialog

--- src/gui/tabular_dialog.py
+++ src/gui/tabular_dialog.py
@@ -1,15 +1,13 @@
 import tkinter as tk
 import tkinter.ttk as ttk
-from tkinter import messagebox
+from tkinter import filedialog, messagebox
 from pygubu.widgets.scrollbarhelper import ScrollbarHelper
 import dataclasses
 import math
 import cmath
 import logging
 
-from lib import Clipboard
-from lib import SParamFile
-from lib import PlotData
+from lib import Clipboard, SParamFile, PlotData, Si
 
 
 
@@ -29,6 +27,11 @@ class TabularDatasetPlot(TabularDataset):
 
 class TabularDialog:
 
+    
+    DISPLAY_PREC = 4
+    EXPORT_PREC  = 8
+
+
     def __init__(self, datasets: list[TabularDataset], initial_selection: int, master=None):
         
         assert initial_selection < len(datasets)
@@ -37,48 +40,54 @@ class TabularDialog:
 
         def get_display_name(item):
             if isinstance(item, TabularDatasetFile):
-                return 'File ' + item.file.name
+                return 'File: ' + item.file.name
             if isinstance(item, TabularDatasetPlot):
-                return 'Plot ' + item.plot.name
+                return 'Plot: ' + item.plot.name
             raise ValueError()
         
-        self.toplevel_info = tk.Tk() if master is None else tk.Toplevel(master)
-        self.toplevel_info.configure(height=600, width=800)
-        self.toplevel_info.title("Tabular Data")
+        self.toplevel_tabular = tk.Tk() if master is None else tk.Toplevel(master)
+        self.toplevel_tabular.configure(height=600, width=800)
+        self.toplevel_tabular.title("Tabular Data")
         
-        self.frame1 = ttk.Frame(self.toplevel_info)
-        self.frame1.configure(padding=10)
-        self.frame1.pack(expand="true", fill="x", side="top")
-        self.combobox_file = ttk.Combobox(self.frame1)
+        self.frame_top = ttk.Frame(self.toplevel_tabular)
+        self.frame_top.configure(padding=10)
+        self.combobox_file = ttk.Combobox(self.frame_top)
         self.combobox_file.configure(state="readonly")
         self.combobox_file.bind("<<ComboboxSelected>>", self.on_select_file, add="")
         self.combobox_file['values'] = [get_display_name(item) for item in self.datasets]
         self.combobox_file.current(initial_selection)
-        self.combobox_file.pack(side='top', expand='true', fill='x')
-        self.combobox_format = ttk.Combobox(self.frame1)
+        self.combobox_file.pack(side='top', fill='x')
+        self.combobox_format = ttk.Combobox(self.frame_top)
         self.combobox_format.configure(state="readonly")
         self.combobox_format.bind("<<ComboboxSelected>>", self.on_change_format, add="")
         self.combobox_format['values'] = ['Mag / dB', 'Mag (linear)', 'Complex', 'Phase / Rad', 'Phase / Deg']
         self.combobox_format.current(0)
         self.combobox_format.pack(side='left')
-        self.button_copy = ttk.Button(self.frame1)
-        self.button_copy.configure(text='Copy')
-        self.button_copy.configure(command=self.on_copy)
-        self.button_copy.pack(side='right')
+        self.frame_top.pack(fill="x", side="top")
         
-        self.frame2 = ttk.Frame(self.toplevel_info)
-        self.frame2.configure(padding=10)
-        self.scrollbarhelper1 = ScrollbarHelper(self.frame2, scrolltype="both")
+        self.frame_main = ttk.Frame(self.toplevel_tabular)
+        self.frame_main.configure(padding=10)
+        self.scrollbarhelper1 = ScrollbarHelper(self.frame_main, scrolltype="both")
         self.scrollbarhelper1.configure(usemousewheel=False)
         self.listbox = ttk.Treeview(self.scrollbarhelper1.container, columns=[])
-        self.listbox.pack(expand="true", fill="both", side="top")
         self.listbox.column("#0", width=0, stretch=tk.NO)
         self.listbox.heading("#0", text="", anchor=tk.W)
+        self.listbox.pack(fill="both", side="top")
         self.scrollbarhelper1.add_child(self.listbox)
-        self.scrollbarhelper1.pack(expand="true", fill="both", side="bottom")
-        self.frame2.pack(expand="true", fill="both", side="top")
+        self.scrollbarhelper1.pack(fill="both", side="top", expand='true')
+        self.frame_main.pack(fill='both', side="top", expand='true')
         
-        self.mainwindow = self.toplevel_info
+        self.menu_main = tk.Menu(self.toplevel_tabular)
+        self.submenu_file = tk.Menu(self.menu_main, tearoff="false")
+        self.menu_main.add(tk.CASCADE, menu=self.submenu_file, label='File')
+        self.submenu_file.add("command", command=self.on_save_single, label='Save...')
+        #self.submenu_file.add("command", command=self.on_save_all, label='Save All...')
+        self.submenu_edit = tk.Menu(self.menu_main)
+        self.menu_main.add(tk.CASCADE, menu=self.submenu_edit, label='Edit')
+        self.submenu_edit.add("command", command=self.on_copy, label='Copy')
+        self.toplevel_tabular.configure(menu=self.menu_main)
+        
+        self.mainwindow = self.toplevel_tabular
         
         self.update_data()
     
@@ -120,7 +129,6 @@ class TabularDialog:
             row = [f]
             for ep in range(file.nw.nports):
                 for ip in range(file.nw.nports):
-                    #row.append(f'\t{v2db(file.nw.s[i,ep-1,ip-1]):+.4g} dB')
                     row.append(file.nw.s[i,ep-1,ip-1])
             rows.append(row)
         
@@ -143,31 +151,91 @@ class TabularDialog:
 
         self.listbox['columns'] = cols
         for col in cols:
-            self.listbox.column(col, anchor=tk.E, width=120)
+            self.listbox.column(col, anchor=tk.E, width=100, minwidth=60, stretch=0)
             self.listbox.heading(col, text=col, anchor=tk.E)
 
-        f_formatter = lambda f,u: f'{f/1e9:.4g} GHz' if u else f'{f:.4g}'
-
-        format_selection = self.combobox_format.current()
-        if format_selection==0:
-            formatter = lambda x,u: f'{20*math.log10(max(1e-15,abs(x))):+.4g}' + (' dB' if u else '')
-        elif format_selection==1:
-            formatter = lambda x,u: f'{abs(x):.4g}'
-        elif format_selection==2:
-            formatter = lambda x,u: f'{x.real:.4g}{x.imag:+.4g}j'
-        elif format_selection==3:
-            formatter = lambda x,u: f'{cmath.phase(x):4g}'
-        elif format_selection==4:
-            formatter = lambda x,u: f'{cmath.phase(x)*math.pi/180:.4g}' + ('°' if u else '')
-        else:
-            raise ValueError(f'Invalid combobox selection: {format}')
+        xfmt_disp, yfmt_disp = self.get_formatters(for_display=True)
+        xfmt_copy, yfmt_copy = self.get_formatters(for_display=False)
         
         self.listbox.delete(*self.listbox.get_children())
         self.clipboard_data = '\t'.join(cols) + '\n'
         for row in rows:
-            formatted_row = [f_formatter(row[0],True)] + ['\t'+formatter(x,True) for x in row[1:]]
-            self.listbox.insert('', 'end', values=formatted_row)
-            self.clipboard_data += '\t'.join([f_formatter(row[0],False)] + [formatter(x,False) for x in row[1:]]) + '\n'
+            display_row   = [xfmt_disp(row[0])] + ['\t'+yfmt_disp(y) for y in row[1:]]
+            clipboard_row = [xfmt_copy(row[0])] + ['\t'+yfmt_copy(y) for y in row[1:]]
+            self.listbox.insert('', 'end', values=display_row)
+            self.clipboard_data += '\t'.join(clipboard_row) + '\n'
+    
+    
+    def get_formatters(self, for_display: bool):
+
+        prec = TabularDialog.DISPLAY_PREC if for_display else TabularDialog.EXPORT_PREC
+        
+        x_formatter = lambda x: str(Si(x, significant_digits=prec))
+        
+        format_selection = self.combobox_format.current()
+        if format_selection==0:
+            y_formatter = lambda y: f'{20*math.log10(max(1e-15,abs(y))):+.{prec}g}'
+        elif format_selection==1:
+            y_formatter = lambda y: str(Si(abs(y), significant_digits=prec))
+        elif format_selection==2:
+            def complex_formatter(y):
+                if y.imag==0:
+                    return f'{y.real:.{prec}g}'
+                if y.real==0:
+                    return f'{y.imag:.{prec}g}j'
+                return f'{y.real:.{prec}g}{y.imag:+.{prec}g}j'
+            y_formatter = complex_formatter
+        elif format_selection==3:
+            y_formatter = lambda y: f'{cmath.phase(y):.{prec}g}'
+        elif format_selection==4:
+            y_formatter = lambda y: f'{cmath.phase(y)*math.pi/180:.{prec}g}'
+        else:
+            raise ValueError(f'Invalid combobox selection: {format}')
+        
+        return x_formatter, y_formatter
+
+
+    def on_save_single(self):
+        self.save(all_datasets=False)
+
+    def on_save_all(self):
+        self.save(all_datasets=True)
+
+
+    def save(self, all_datasets: bool = False):
+        if all_datasets:
+            filename = filedialog.asksaveasfilename(
+                title='Save All Tabular Data', confirmoverwrite=True, defaultextension='.xlsx',
+                filetypes=(
+                    ('Spreadsheet','.xlsx'),
+                    ('All Files','*'),
+                ))
+        else:
+            filename = filedialog.asksaveasfilename(
+                title='Save Tabular Data', confirmoverwrite=True, defaultextension='.csv',
+                filetypes=(
+                    ('CSV','.csv'),
+                    ('Spreadsheet','.xlsx'),
+                    ('All Files','*'),
+                ))
+        
+        if not filename:
+            return
+
+        if filename.lower().endswith('xlsx') or all_datasets:
+            self.save_spreadsheet(filename, all_datasets=all_datasets)
+        else:
+            self.save_csv(filename)
+
+
+    def save_csv(self, filename):
+        raise NotImplementedError()
+
+
+    def save_spreadsheet(self, filename, all_datasets: bool = False):
+        if all_datasets:
+            raise NotImplementedError()
+        raise NotImplementedError()
 
 
     def on_copy(self):
