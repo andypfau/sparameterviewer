efb2523456c1d82fbbda2b8219733df3bfae5907
Andreas Pfau <andreas.pfau@apfau.de>
2024-07-22 11:17:35 +0200

    Worked on export dialog

--- src/gui/tabular_dialog.py
+++ src/gui/tabular_dialog.py
@@ -6,22 +6,66 @@ import dataclasses
 import math
 import cmath
 import logging
+import io
+import pandas as pd
 
-from lib import Clipboard, SParamFile, PlotData, Si
+from lib import Clipboard, SParamFile, PlotData, Si, AppGlobal
 
 
 
 class TabularDataset:
-    def display_name(self):
+    @property
+    def name(self) -> str:
+        raise NotImplementedError()
+    @property
+    def columns(self) -> list[str]:
+        raise NotImplementedError()
+    @property
+    def rows(self) -> list[list[complex]]:
         raise NotImplementedError()
     
 @dataclasses.dataclass
 class TabularDatasetFile(TabularDataset):
     file: SParamFile
+    @property
+    def name(self) -> str:
+        return self.file.name
+    @property
+    def columns(self) -> list[str]:
+        cols = ['Frequency']
+        for ep in range(self.file.nw.nports):
+            for ip in range(self.file.nw.nports):
+                cols.append(f'S{ep+1},{ip+1}')
+        return cols
+    @property
+    def rows(self) -> list[list[complex]]:
+        rows = []
+        for i,f in enumerate(self.file.nw.f):
+            row = [f]
+            for ep in range(self.file.nw.nports):
+                for ip in range(self.file.nw.nports):
+                    row.append(self.file.nw.s[i,ep-1,ip-1])
+            rows.append(row)
+        return rows
     
 @dataclasses.dataclass
 class TabularDatasetPlot(TabularDataset):
     plot: PlotData
+    @property
+    def name(self) -> str:
+        return self.plot.name
+    @property
+    def columns(self) -> list[str]:
+        if self.plot.z is not None:
+            return [self.plot.x.name, self.plot.y.name, self.plot.z.name]
+        else:
+            return [self.plot.x.name, self.plot.y.name]
+    @property
+    def rows(self) -> list[list[complex]]:
+        if self.plot.z is not None:
+            return [[x,y,z] for (x,y,z) in zip(self.plot.x.values, self.plot.y.values, self.plot.z.values)]
+        else:
+            return [[x,y] for (x,y) in zip(self.plot.x.values, self.plot.y.values)]
     
 
 
@@ -36,18 +80,18 @@ class TabularDialog:
         
         assert initial_selection < len(datasets)
         self.datasets = datasets
-        self.clipboard_data = ''
 
         def get_display_name(item):
             if isinstance(item, TabularDatasetFile):
-                return 'File: ' + item.file.name
+                return 'File: ' + item.name
             if isinstance(item, TabularDatasetPlot):
-                return 'Plot: ' + item.plot.name
+                return 'Plot: ' + item.name
             raise ValueError()
         
         self.toplevel_tabular = tk.Tk() if master is None else tk.Toplevel(master)
         self.toplevel_tabular.configure(height=600, width=800)
         self.toplevel_tabular.title("Tabular Data")
+        AppGlobal.set_toplevel_icon(self.toplevel_tabular)
         
         self.frame_top = ttk.Frame(self.toplevel_tabular)
         self.frame_top.configure(padding=10)
@@ -81,7 +125,7 @@ class TabularDialog:
         self.submenu_file = tk.Menu(self.menu_main, tearoff="false")
         self.menu_main.add(tk.CASCADE, menu=self.submenu_file, label='File')
         self.submenu_file.add("command", command=self.on_save_single, label='Save...')
-        #self.submenu_file.add("command", command=self.on_save_all, label='Save All...')
+        self.submenu_file.add("command", command=self.on_save_all, label='Save All...')
         self.submenu_edit = tk.Menu(self.menu_main)
         self.menu_main.add(tk.CASCADE, menu=self.submenu_edit, label='Edit')
         self.submenu_edit.add("command", command=self.on_copy, label='Copy')
@@ -102,145 +146,188 @@ class TabularDialog:
     
     def on_select_file(self, event=None):
         self.update_data()
-    
-
-    def update_data(self):
-        
-        if len(self.datasets) < 1:
-            return
-        
-        item = self.datasets[self.combobox_file.current()]
-        if isinstance(item, TabularDatasetFile):
-            self.plot_sparam_file(item.file)
-        elif isinstance(item, TabularDatasetPlot):
-            self.plot_plot_data(item.plot)
-        else:
-            raise ValueError()
-        
-    
-    def plot_sparam_file(self, file: SParamFile):
-
-        cols = ['Frequency']
-        for ep in range(file.nw.nports):
-            for ip in range(file.nw.nports):
-                cols.append(f'S{ep+1},{ip+1}')
-        rows = []
-        for i,f in enumerate(file.nw.f):
-            row = [f]
-            for ep in range(file.nw.nports):
-                for ip in range(file.nw.nports):
-                    row.append(file.nw.s[i,ep-1,ip-1])
-            rows.append(row)
-        
-        self.plot_data(cols, rows)
-    
-    
-    def plot_plot_data(self, data: PlotData):
-
-        if data.z is not None:
-            cols = [data.x.name, data.y.name, data.z.name]
-            rows = [[x,y,z] for (x,y,z) in zip(data.x.values, data.y.values, data.z.values)]
-        else:
-            cols = [data.x.name, data.y.name]
-            rows = [[x,y] for (x,y) in zip(data.x.values, data.y.values)]
-        
-        self.plot_data(cols, rows)
-
-        
-    def plot_data(self, cols: list[str], rows: list[list[complex]]):
-
-        self.listbox['columns'] = cols
-        for col in cols:
-            self.listbox.column(col, anchor=tk.E, width=100, minwidth=60, stretch=0)
-            self.listbox.heading(col, text=col, anchor=tk.E)
-
-        xfmt_disp, yfmt_disp = self.get_formatters(for_display=True)
-        xfmt_copy, yfmt_copy = self.get_formatters(for_display=False)
-        
-        self.listbox.delete(*self.listbox.get_children())
-        self.clipboard_data = '\t'.join(cols) + '\n'
-        for row in rows:
-            display_row   = [xfmt_disp(row[0])] + ['\t'+yfmt_disp(y) for y in row[1:]]
-            clipboard_row = [xfmt_copy(row[0])] + ['\t'+yfmt_copy(y) for y in row[1:]]
-            self.listbox.insert('', 'end', values=display_row)
-            self.clipboard_data += '\t'.join(clipboard_row) + '\n'
-    
-    
-    def get_formatters(self, for_display: bool):
-
-        prec = TabularDialog.DISPLAY_PREC if for_display else TabularDialog.EXPORT_PREC
-        
-        x_formatter = lambda x: str(Si(x, significant_digits=prec))
-        
-        format_selection = self.combobox_format.current()
-        if format_selection==0:
-            y_formatter = lambda y: f'{20*math.log10(max(1e-15,abs(y))):+.{prec}g}'
-        elif format_selection==1:
-            y_formatter = lambda y: str(Si(abs(y), significant_digits=prec))
-        elif format_selection==2:
-            def complex_formatter(y):
-                if y.imag==0:
-                    return f'{y.real:.{prec}g}'
-                if y.real==0:
-                    return f'{y.imag:.{prec}g}j'
-                return f'{y.real:.{prec}g}{y.imag:+.{prec}g}j'
-            y_formatter = complex_formatter
-        elif format_selection==3:
-            y_formatter = lambda y: f'{cmath.phase(y):.{prec}g}'
-        elif format_selection==4:
-            y_formatter = lambda y: f'{cmath.phase(y)*math.pi/180:.{prec}g}'
-        else:
-            raise ValueError(f'Invalid combobox selection: {format}')
-        
-        return x_formatter, y_formatter
-
-
-    def on_save_single(self):
-        self.save(all_datasets=False)
-
-    def on_save_all(self):
-        self.save(all_datasets=True)
-
-
-    def save(self, all_datasets: bool = False):
-        if all_datasets:
-            filename = filedialog.asksaveasfilename(
-                title='Save All Tabular Data', confirmoverwrite=True, defaultextension='.xlsx',
-                filetypes=(
-                    ('Spreadsheet','.xlsx'),
-                    ('All Files','*'),
-                ))
-        else:
-            filename = filedialog.asksaveasfilename(
-                title='Save Tabular Data', confirmoverwrite=True, defaultextension='.csv',
-                filetypes=(
-                    ('CSV','.csv'),
-                    ('Spreadsheet','.xlsx'),
-                    ('All Files','*'),
-                ))
-        
-        if not filename:
-            return
-
-        if filename.lower().endswith('xlsx') or all_datasets:
-            self.save_spreadsheet(filename, all_datasets=all_datasets)
-        else:
-            self.save_csv(filename)
-
-
-    def save_csv(self, filename):
-        raise NotImplementedError()
-
-
-    def save_spreadsheet(self, filename, all_datasets: bool = False):
-        if all_datasets:
-            raise NotImplementedError()
-        raise NotImplementedError()
 
 
     def on_copy(self):
-        try:
-            Clipboard.copy_string(self.clipboard_data)
-        except Exception as ex:
-            logging.exception(f'Copying CSV-data to clipboard failed: {ex}')
-            messagebox.showerror('Error', str(ex))
+        dataset = self.selected_dataset
+        if dataset is None:
+            return
+        self.copy_data(dataset)
+
+
+    def on_save_single(self):
+        dataset = self.selected_dataset
+        if dataset is None:
+            return
+
+        filename = filedialog.asksaveasfilename(
+            title='Save Tabular Data', confirmoverwrite=True, defaultextension='.csv',
+            filetypes=(
+                ('CSV','.csv'),
+                ('Spreadsheet','.xlsx'),
+                ('All Files','*'),
+            ))
+        if not filename:
+            return
+
+        if filename.lower().endswith('.xlsx'):
+            self.save_spreadsheet(dataset, filename)
+        else:
+            self.save_csv(dataset, filename)
+        
+
+    def on_save_all(self):
+        if len(self.datasets) < 1:
+            return
+
+        filename = filedialog.asksaveasfilename(
+            title='Save All Tabular Data', confirmoverwrite=True, defaultextension='.xlsx',
+            filetypes=(
+                ('Spreadsheet','.xlsx'),
+                ('All Files','*'),
+            ))
+        if not filename:
+            return
+
+        self.save_spreadsheets_all(filename)
+    
+
+    def update_data(self):
+        dataset = self.selected_dataset
+        if dataset is None:
+            return
+        self.plot_data(dataset)
+    
+
+    @property
+    def selected_dataset(self) -> TabularDataset:
+        if len(self.datasets) < 1:
+            return None
+        return self.datasets[self.combobox_file.current()]
+
+        
+    def plot_data(self, dataset: "TabularDataset"):
+
+        x_convert, x_format = self.get_x_formatter()
+        _, y_convert, y_format, y_unit = self.get_y_formatter()
+        
+        self.listbox['columns'] = dataset.columns
+        for col in dataset.columns:
+            unit = '' if (y_unit is None) else (' / ' + y_unit)
+            self.listbox.column(col, anchor=tk.E, width=100, minwidth=60, stretch=0)
+            self.listbox.heading(col, text=col+unit, anchor=tk.E)
+
+        self.listbox.delete(*self.listbox.get_children())
+        for row in dataset.rows:
+            display_row   = [x_format(x_convert(row[0]))] + ['\t'+y_format(y_convert(y)) for y in row[1:]]
+            self.listbox.insert('', 'end', values=display_row)
+
+        
+    def copy_data(self, dataset: "TabularDataset"):
+
+        df = self.get_dataframe(dataset)
+        df.to_clipboard(index=False)
+    
+
+    def get_dataframe(self, dataset: "TabularDataset") -> "pd.DataFrame":
+
+        x_convert, _ = self.get_x_formatter()
+        is_re_im, y_convert, _, y_unit = self.get_y_formatter()
+
+        processed_cols = [dataset.columns[0]]
+        for col in dataset.columns[1:]:
+            unit = '' if (y_unit is None) else (' / ' + y_unit)
+            if is_re_im:
+                processed_cols.append(col + ' real' + unit)
+                processed_cols.append(col + ' imag' + unit)
+            else:
+                processed_cols.append(col + unit)
+        
+        processed_rows = []
+        for row in dataset.rows:
+            col_data = [x_convert(row[0])]
+            for col in row[1:]:
+                if is_re_im:
+                    (re,im) = y_convert(col)
+                    col_data.append(re)
+                    col_data.append(im)
+                else:
+                    col_data.append(y_convert(col))
+            processed_rows.append(col_data)
+        
+        return pd.DataFrame(processed_rows, columns=processed_cols)
+
+
+    def save_csv(self, dataset: "TabularDataset", filename: str):
+        df = self.get_dataframe(dataset)
+        df.to_csv(filename, sep='\t', index=None)
+
+
+    def save_spreadsheet(self, dataset: "TabularDataset", filename: str):
+        df = self.get_dataframe(dataset)
+        df.to_excel(filename, sheet_name=dataset.name, index=None, freeze_panes=[1,0])
+
+
+    def save_spreadsheets_all(self, filename: str):
+        names = [dataset.name for dataset in self.datasets]
+        dataframes = [self.get_dataframe(dataset) for dataset in self.datasets]
+
+        writer = pd.ExcelWriter(filename)
+        for name,df in zip(names,dataframes):
+            df.to_excel(writer, sheet_name=name, index=None, freeze_panes=[1,0])
+        writer.close()
+    
+
+    def get_x_formatter(self):
+        
+        prec = TabularDialog.DISPLAY_PREC
+        
+        converter = lambda value: value
+        formatter = lambda value: str(Si(value, significant_digits=prec))
+
+        return converter, formatter
+    
+
+    def get_y_formatter(self):
+        
+        prec = TabularDialog.DISPLAY_PREC
+        format_selection = self.combobox_format.current()
+        is_re_im = False
+
+        if format_selection==0: # dB
+            converter = lambda value: 20*math.log10(max(1e-15,abs(value)))
+            formatter = lambda db: f'{db:.{prec}g}'
+            unit = 'dB'
+
+        elif format_selection==1: # linear
+            converter = lambda value: value
+            formatter = lambda linval: str(Si(linval, significant_digits=prec))
+            unit = None
+
+        elif format_selection==2: # complex
+            is_re_im = True
+            converter = lambda value: (value.real, value.imag)
+            def complex_formatter(value):
+                (re, im) = value
+                if im==0:
+                    return f'{re:.{prec}g}'
+                if re==0:
+                    return f'{im:.{prec}g}j'
+                return f'{re:.{prec}g}{im:+.{prec}g}j'
+            formatter = complex_formatter
+            unit = None
+        
+        elif format_selection==3: # rad
+            converter = lambda value: cmath.phase(value)
+            formatter = lambda rad: f'{rad:.{prec}g}'
+            unit = 'rad'
+        
+        elif format_selection==4: # degrees
+            converter = lambda value: cmath.phase(value)*180/math.pi
+            formatter = lambda deg: f'{deg:.{prec}g}'
+            unit = 'deg'
+        
+        else:
+            raise ValueError(f'Invalid combobox selection: {format}')
+    
+        return is_re_im, converter, formatter, unit
