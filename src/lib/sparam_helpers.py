Fixed TDR units

src/lib/sparam_helpers.py
@@ -89,17 +89,18 @@ def sparam_to_timedomain(f: np.ndarray, spar: np.ndarray, *, shift: float = 0.0,
     while next_pow_of_2 < max(len(sp_windowed), min_size):
         next_pow_of_2 *= 2
     n_missing = next_pow_of_2 - len(sp_windowed)
-    frequency_scaling = next_pow_of_2 / len(sp_windowed)  # padding increases the highest frequency!
+    correction_factor = next_pow_of_2 / len(sp_windowed)  # padding increases the highest frequency!
     sp_padded = np.concatenate([sp_windowed, np.zeros([n_missing], dtype=sp_windowed.dtype)])
 
-    f_nyq = max(f) * frequency_scaling
+    f_nyq = max(f) * correction_factor
     f_sa = 2.0 * f_nyq
 
     ir_unshifted = np.fft.irfft(sp_padded)
+    ir_unshifted_gaincorrected = ir_unshifted * correction_factor
     
     sa_period = 1.0 / f_sa
     n_shift = round(shift / sa_period)
-    ir_shifted = np.roll(ir_unshifted, n_shift)
+    ir_shifted = np.roll(ir_unshifted_gaincorrected, n_shift)
     ir = ir_shifted[:len(ir_shifted)//2+n_shift]
     
     t_tot = (len(ir_unshifted)-1) * sa_period
@@ -107,7 +108,7 @@ def sparam_to_timedomain(f: np.ndarray, spar: np.ndarray, *, shift: float = 0.0,
     t -= shift
 
     if step_response:
-        sr = np.cumsum(ir)
+        sr = np.cumsum(ir) / correction_factor
         return t, sr
     else:
         return t, ir