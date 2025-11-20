3a69e41c19ec0a4dbd55269c61c0a86ec60a0f61
Andreas Pfau <andreas.pfau@apfau.de>
2025-11-20 08:26:10 +0100

    wording

--- src/gui/helpers/help.py
+++ src/gui/helpers/help.py
@@ -10,9 +10,9 @@ def show_help(doc: str = 'main.md'):
         if not helpfile_path.exists():
             raise RuntimeError(f'<{str(helpfile_path)}> not exists')
     except Exception as ex:
-        error_dialog('Unable to locate documentation', 'Unable to locate documentation.', f'Try to locate <sparameterviewer/docs> yourself ({ex}).')
+        error_dialog('Unable to locate documentation', 'Unable to locate documentation.', f'Try to locate <sparameterviewer/docs> manually ({ex}).')
     
     try:
         open_file_in_default_viewer(str(helpfile_path))
     except Exception as ex:
-        error_dialog('Unable to show documentation', f'Unable to show documentation.', f'Try to open <{str(helpfile_path)}> yourself ({ex}).')
+        error_dialog('Unable to show documentation', f'Unable to show documentation.', f'Try to open <{str(helpfile_path)}> manually ({ex}).')
