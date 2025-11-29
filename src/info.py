febe8b095d1051e3901f85f013961c3345980c75
Andreas Pfau <andreas.pfau@apfau.de>
2025-11-29 09:55:12 +0100

    version number

--- src/info.py
+++ src/info.py
@@ -2,5 +2,5 @@ class Info:
     Author = 'Andreas Pfau'
     Domain = 'apfau.de'
     AppName = 'S-Parameter Viewer'
-    AppVersionStr = "0.40b31"  # see https://peps.python.org/pep-0440/
-    AppDateStr = "2025-11-20"
+    AppVersionStr = "0.41b1"  # see https://peps.python.org/pep-0440/
+    AppDateStr = "2025-11-27"
